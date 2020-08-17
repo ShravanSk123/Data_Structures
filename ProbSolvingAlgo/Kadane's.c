cf74c56f99f1e1f2d57176ffa057fa2d7fdcdb01
ShravanSk123 <46959596+ShravanSk123@users.noreply.github.com>
2020-08-17 11:12:25 +0530

    Finds max sum in sub array and prints sub array

--- ProbSolvingAlgo/Kadane's.c
+++ ProbSolvingAlgo/Kadane's.c
@@ -10,13 +10,25 @@ void main()
         scanf("%d",&a[i]);
     int max_sofar=a[0]; //this holds final answer
     int new_max=0;
+    int start=0,end=0,s=0;
     for(int i=0;i<n;i++)
     {
         new_max += a[i];
         if(max_sofar<new_max)
+        {
             max_sofar = new_max;
+            start = s;
+            end = i;
+        }
         if(new_max<0)
+        {
             new_max=0;
+            s = i+1; // subarray starting index changes
+        }
     }
     printf("The maximum subarray sum is %d\n",max_sofar);
+    printf("The subarray is\n");
+    for(int i=start;i<=end;i++)
+        printf("%d ",a[i]);
+    printf("\n");
 }
